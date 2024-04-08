2dbe55a79d02d038acfcf125a343f9f2edcfcb03
unknown <rishavranjananan@gmail.com>
2024-04-08 17:36:57 +0530

    Shifting local to online

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
-
+const PORT = process.env.PORT || 3000;
 
 const person = require('./models/person'); 
 const menu = require('./models/menu'); 
@@ -23,6 +25,8 @@ const personRoutes = require('./routes/personRoutes');
 app.use('/menu', menuItemRoutes);
 app.use('/person', personRoutes);
 
-app.listen(3000, ()=>{
+
+
+app.listen(PORT, ()=>{
     console.log('Server Zinda hai Bhai')
 })
